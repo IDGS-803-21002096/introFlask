725cd0394bc12150f896be20b519d36367e302f6
21002096@alumnos.utleon.edu.mx <jr960219@gmail.com>
2025-01-30 23:53:02 -0600

    formularios

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__) #Se crea la aplicacion como tal
 
 @app.route('/')
 def index():
-    return "Hola Mundo!!!"
+    grupo="IDGS803"
+    lista = ["Juan", "Pedro", "Mario"]
+    return render_template("index.html", grupo=grupo, lista=lista) #Abre el archivo html con el render
+
+@app.route('/OperasBas', methods=["GET","POST"])
+def operas():
+    resultado = None
+    if request.method == "POST":
+        num1 = request.form.get("n1")
+        num2 = request.form.get("n2")
+        resultado = "La suma de {} + {} es {}".format(num1, num2,int(num1)+int(num2))
+    return render_template("OperasBas.html", resultado=resultado)
+
+@app.route('/resultado', methods=["GET","POST"])
+def resultado():
+    if request.method == "POST":
+        num1 = request.form.get("n1")
+        num2 = request.form.get("n2")
+        return "La suma de {} + {} es {}".format(num1, num2,int(num1)+int(num2))
+
+@app.route('/ejemplo1')
+def ejemplo1():
+    return render_template("ejemplo1.html")
+
+@app.route('/ejemplo2')
+def ejemplo2():
+    return render_template("ejemplo2.html")
 
 @app.route('/hola')
 def hola():
@@ -30,6 +56,22 @@ def suma(n1,n2):
 @app.route('/default/<string:nom>')
 def func(nom = 'pedro'):
     return "El nombre de nom es "+nom
+
+@app.route('/form1')
+def form1():
+    return '''
+        <form>
+        <label>Nombre: </label>
+        <input type="text" name="nombre" placeholder="nombre">
+        </br>
+        <form>
+        <label>Nombre: </label>
+        <input type="text" name="nombre" placeholder="nombre">
+        </br>
+        </form>
+    '''
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
